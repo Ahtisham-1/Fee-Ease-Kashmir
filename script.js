9184837429f1f5f9abce4e3a689c20e60c4caeed
Ahtisham01 <219668890+Ahtisham-1@users.noreply.github.com>
2026-06-28 16:50:31 +0530

    feat: complete student dropdown selection and dynamic balances calculation

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ const adminButton = document.getElementById("adminButton");
 const parentDashboard = document.getElementById("parentDashboard");
 const adminDashboard = document.getElementById("adminDashboard");
 
+//section 1 parent elements
+const studentSelect = document.getElementById("studentSelect");
+let totalOwned = document.getElementById("totalOwned");
+let totalPaid = document.getElementById("totalPaid");
+let netBalanceDue = document.getElementById("netBalanceDue");
+
+//Active parent by default
+let activeParentId = "P1";
+
+//by default we hide the admin dashboard
+adminDashboard.classList.add("hidden");
+
 //Arrays we store inside them if our local storage has any data inside it then show that if not then fall back to dummy data
 let parents = JSON.parse(localStorage.getItem("parents")) || [
   { id: "P1", name: "Shabir Ahmad" },
@@ -15,10 +27,10 @@ let students = JSON.parse(localStorage.getItem("students")) || [
   { id: "S3", name: "Mehnan", parentId: "P2" },
 ];
 
-let fees = JSON.parse(localStorage.getItem("fees")) || [
+let studentfees = JSON.parse(localStorage.getItem("studentfees")) || [
   {
     studentID: "S1",
-    fees: 1500,
+    fees: 1505550,
     month: "June",
     feesType: "Tution",
     feesId: "F1",
@@ -45,12 +57,52 @@ let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
 function saveData() {
   localStorage.setItem("parents", JSON.stringify(parents));
   localStorage.setItem("students", JSON.stringify(students));
-  localStorage.setItem("fees", JSON.stringify(fees));
+  localStorage.setItem("studentfees", JSON.stringify(studentfees));
   localStorage.setItem("transactions", JSON.stringify(transactions));
 }
 
-//by default we hide the admin dashboard
-adminDashboard.classList.add("hidden");
+// Function for loading the students in parent dashboard
+function loadStudentDropdown() {
+  const activeStudent = students.filter(
+    (elements) => elements.parentId === activeParentId,
+  );
+  activeStudent.forEach((student) => {
+    const optionElement = document.createElement("option");
+    optionElement.value = student.id;
+    optionElement.textContent = student.name;
+    studentSelect.appendChild(optionElement);
+  });
+}
+loadStudentDropdown();
+
+//Filter function for showing what each student has paid owns and their net balance
+
+function updateBalances() {
+  let balanceOwned = 0;
+  let balancePaid = 0;
+
+  const selectedStudentId = studentSelect.value;
+  studentfees.forEach((feeItem) => {
+    if (feeItem.studentID === selectedStudentId) {
+      balanceOwned = balanceOwned + feeItem.fees;
+    }
+  });
+
+  transactions.forEach((transactionItem) => {
+    if (transactionItem.studentID === selectedStudentId) {
+      balancePaid = balancePaid + transactionItem.paid;
+    }
+  });
+
+  let balanceDue = balanceOwned - balancePaid;
+
+  totalOwned.textContent = balanceOwned;
+  totalPaid.textContent = balancePaid;
+  netBalanceDue.textContent = balanceDue;
+}
+
+updateBalances();
+studentSelect.addEventListener("change", updateBalances);
 
 //These buttons show whatever button we click its dashboard and hide the other one
 parentButton.addEventListener("click", function () {
